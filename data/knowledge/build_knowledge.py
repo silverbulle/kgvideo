931e7e665ae962bfe6a87817af1b5535f76a5ab1
silverbulle <904666694@qq.com>
2021-09-01 17:03:01 +0800

    base finish id config

--- data/knowledge/build_knowledge.py
+++ data/knowledge/build_knowledge.py
@@ -6,63 +6,96 @@ target = '../MSR-VTT/OPENKE_file/entity_total_try.txt'
 entity2id = '../MSR-VTT/OPENKE_file/msrvtt/entity.txt'
 relation2id = '../MSR-VTT/OPENKE_file/msrvtt/relation2id.txt'
 total = '../MSR-VTT/OPENKE_file/msrvtt/total.txt'
+train = '../MSR-VTT/OPENKE_file/msrvtt/train.txt'
+test = '../MSR-VTT/OPENKE_file/msrvtt/test.txt'
+valid = '../MSR-VTT/OPENKE_file/msrvtt/valid.txt'
 
 f1 = open(rel_induction, 'r')
 f2 = open(ent_induction, 'r')
 t = open(target, 'r')
 # w1 = open(relation2id, 'w')
 # w2 = open(entity2id, 'w')
+# r1 = open(relation2id, 'r')
+# r2 = open(entity2id, 'r')
+####################
+# w3 = open(total, 'w')
+#
+# rel_induction_lines = f1.readlines()
+# ent_induction_lines = f2.readlines()
+# target_lines = t.readlines()
+# # ur1 = r1.readlines()
+# # ur2 = r2.readlines()
+#
+# for target_line in tqdm(target_lines):
+#     list1 = target_line.split('&')
+#     for i in range(len(list1)):
+#         if i == 0 or i == 1:
+#             no_match = True
+#             for line in ent_induction_lines:
+#                 if not no_match:  # avoid repeat match string
+#                     break
+#                 step1 = line.split('|')
+#
+#                 step2 = step1[1].replace('\n', '').split('#')
+#                 # if list1[i].strip() in step2:
+#                 for j in step2:
+#                     if j in list1[i].strip() and j != '':
+#                         w3.write(step1[0] + ',')
+#                         no_match = False
+#                         break
+#
+#                 # print(step2)
+#             if no_match:
+#                 w3.write('none,')
+#             # w1.close()
+#         elif i == 2:
+#             no_match = True
+#             for line in rel_induction_lines:
+#                 if not no_match:
+#                     break
+#                 step1 = line.split('|')
+#                 step2 = step1[1].replace('\n', '').split('#')
+#                 for j in step2:
+#                     if j in list1[i].strip():
+#                         w3.write(step1[0] + '\n')
+#                         no_match = False
+#                         break
+#             if no_match:
+#                 w3.write('none\n')
+#
+#             # w2.close()
+# w3.close()
+############
+# tran total.txt to id
 r1 = open(relation2id, 'r')
 r2 = open(entity2id, 'r')
-w3 = open(total, 'w')
+r3 = open(total, 'r')
+w_train = open(train, 'w')
+w_test = open(test, 'w')
+w_valid = open(valid, 'w')
 
-rel_induction_lines = f1.readlines()
-ent_induction_lines = f2.readlines()
-target_lines = t.readlines()
-ur1 = r1.readlines()
-ur2 = r2.readlines()
-
-for target_line in tqdm(target_lines):
-    list1 = target_line.split('&')
-    for i in range(len(list1)):
+relid = r1.readlines()
+entid = r2.readlines()
+reldict = {}
+entdict = {}
+for line in relid:
+    rel_id = line.split(' ')
+    # print(rel_id)
+    reldict[rel_id[0]] = int(rel_id[-1].replace('\n', ''))
+for line in entid:
+    ent_id = line.split('       ')
+    entdict[ent_id[0]] = int(ent_id[-1].replace('\n', ''))
+print(reldict, '\n', entdict)
+r1.close()
+r2.close()
+total = r3.readlines()
+for line in tqdm(total):
+    word = line.split(',')
+    word[2] = word[2].replace('\n', '')
+    if word[0] == 'none' or word[1] == 'none' or word[2] == 'none':
+        continue
+    for i in range(3):
         if i == 0 or i == 1:
-            no_match = True
-            for line in ent_induction_lines:
-                step1 = line.split('|')
-
-                # w1.writelines("{0:100}{1}".format(step1[0], cnt))  # write rel2id
-                # w1.writelines('\n')
-                # cnt += 1
-
-                step2 = step1[1].replace('\n', '').split('#')
-                # if list1[i].strip() in step2:
-                for j in step2:
-                    if j in list1[i].strip() and j != '':
-                        w3.write(step1[0] + ',')
-                        no_match = False
-                        break
-                # else:
-                #     break
-                # print(step2)
-            if no_match:
-                w3.write('none,')
-            # w1.close()
+            w_train.write(str(entdict[word[i]]) + ',')
         elif i == 2:
-            no_match = True
-            for line in rel_induction_lines:
-                step1 = line.split('|')
-                step2 = step1[1].replace('\n', '').split('#')
-                for j in step2:
-                    if j in list1[i].strip():
-                        w3.write(step1[0] + '\n')
-                        no_match = False
-                        break
-                # else:
-                #     break
-            if no_match:
-                w3.write('none\n')
-                # w2.writelines("{0:100}{1}".format((step1[0]), cnt)) # write ent2id
-                # w2.writelines('\n')
-                # cnt += 1
-
-            # w2.close()
+            w_train.write(str(reldict[word[i]]) + '\n')
